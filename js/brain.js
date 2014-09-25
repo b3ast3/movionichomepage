386aa4ae08e36f1c2c6586319e78a74bfe0fa1f9
b3ast3 <pradeepchowdary770@gmail.com>
2014-09-25 18:30:00 +0530

    Update brain.js

---
+++ js/brain.js
@@ -0,0 +1,16 @@
+
+
+$('#featuredmovies').bxSlider({
+  minSlides: 1,
+  maxSlides: 8,
+  slideWidth: 220,
+  slideMargin: 20
+});
+
+$('#populartvshows').bxSlider({
+  minSlides: 1,
+  maxSlides: 8,
+  slideWidth: 220,
+  slideMargin: 20,
+ 
+});
