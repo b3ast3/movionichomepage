9f4071e4cd4a28615e5d3343a14a5989a4a533f9
b3ast3 <pradeepchowdary770@gmail.com>
2014-09-25 18:10:21 +0530

    Update min.js

--- js/min/min.js
+++ js/min/min.js
@@ -4,4 +4,19 @@ $(document).ready(function(){
  $('.slider').bxSlider();
 
 
+$('#featuredmovies').bxSlider({
+  minSlides: 1,
+  maxSlides: 8,
+  slideWidth: 220,
+  slideMargin: 20
+});
+
+$('#populartvshows').bxSlider({
+  minSlides: 1,
+  maxSlides: 8,
+  slideWidth: 220,
+  slideMargin: 20,
+ 
+});
+
 });
