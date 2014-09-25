97bb020d8e0d8ac802b3613ee8536805bd3edd00
b3ast3 <pradeepchowdary770@gmail.com>
2014-09-25 16:20:21 +0530

    Update min.js

--- js/min/min.js
+++ js/min/min.js
@@ -1,34 +1,6 @@
 $(document).ready(function(){
 
-  $('.slider').bxSlider();
-$('#moviecategories').bxSlider({
-  minSlides: 1,
-  maxSlides: 8,
-  slideWidth: 220,
-  slideMargin: 20
-});
 
-
-$('#featuredmovies').bxSlider({
-  minSlides: 1,
-  maxSlides: 8,
-  slideWidth: 220,
-  slideMargin: 20
-});
-$('#newtrailers').bxSlider({
-  minSlides: 1,
-  maxSlides: 8,
-  slideWidth: 220,
-  slideMargin: 20,
- 
-});
-$('#populartvshows').bxSlider({
-  minSlides: 1,
-  maxSlides: 8,
-  slideWidth: 220,
-  slideMargin: 20,
- 
-});
 
 $('.topslider').flicker({
 block_text: true, // Will class the title and sub text classes of each list item to show a background colour for easy reading. Can be applied to the containing class for a global effect or to each list item separately.
